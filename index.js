533ab67eccd6d4b124ad837cfcef6712362090c4
Guariko <creatingsomething2021@gmail.com>
2022-06-08 07:09:41 -0300

    Discount section complete

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ function removeClass(element, classElement) {
     element.classList.remove(classElement);
 }
 
+function changeTitle (element, widthSize, IfValue, ElseValue) {
+    if (window.innerWidth >= widthSize) {
+        element.innerHTML = IfValue;
+    }
+
+    else {
+        element.innerHTML = ElseValue;
+    }
+}
+
+// FIXME: Function ends //
+
 // TODO: Header //
 
 const closeHeaderNavBar = document.querySelector(".header .fa-times");
@@ -21,3 +33,20 @@ headerBars.addEventListener("click", e => {
 closeHeaderNavBar.addEventListener("click", e => {
     removeClass(headerNavBar, "active");
 });
+
+// FIXME: Header ends //
+
+// TODO: Discount section //
+
+const discountTitle = document.querySelector(".discount__content h2");
+const discountTitleIfValue = "Immerse yourself in <br/>your music";
+const discountTitleElseValue = "Immerse yourself in your music";
+
+
+changeTitle(discountTitle, 650, discountTitleIfValue, discountTitleElseValue);
+
+window.addEventListener("resize", e => {
+    changeTitle(discountTitle, 650, discountTitleIfValue, discountTitleElseValue);
+});
+
+// FIXME: Discount section ends //
